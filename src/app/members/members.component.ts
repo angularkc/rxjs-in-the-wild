47a3d52540b80debb06162f0e2535853e7385393
Steven Barnhurst <stevenbarnhurst@gmail.com>
2020-02-11 15:49:50 -0600

    Finish step-2

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import {DataService} from '../data.service';
-import {FormControl} from '@angular/forms';
-import {map, shareReplay} from 'rxjs/operators';
-
+import { DataService } from '../data.service';
+import { FormControl } from '@angular/forms';
+import {map, shareReplay, startWith, switchMap} from 'rxjs/operators';
 
 @Component({
   templateUrl: './members.component.html'
@@ -13,7 +12,10 @@ export class MembersComponent {
   filterControl = new FormControl();
   filterControlStream$ = this.filterControl.valueChanges;
 
-  dataSource$ = this.dataService.getMembers('').pipe(shareReplay(1));
+  dataSource$ = this.filterControlStream$.pipe(
+    startWith(''),
+    switchMap(value => this.dataService.getMembers(value).pipe(shareReplay(1)))
+  );
 
   constructor(private dataService: DataService) {}
 }
