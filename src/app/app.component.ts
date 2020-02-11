964444f32ac632fb6df3178f5e04959d01ef36b6
Steven Barnhurst <stevenbarnhurst@gmail.com>
2020-02-11 08:54:50 -0600

    Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  routes = [{
+    label: 'Members',
+    value: 'members'
+  }, {
+    label: 'Messages',
+    value: 'messages'
+  }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
+import { MaterialModule } from './material.module';
 import { MembersComponent } from './members/members.component';
 import { MessagesComponent } from './messages/messages.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SiteFiltersComponent } from './site-filters/site-filters.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes: Routes = [
+  {
+    path: 'members',
+    component: MembersComponent
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'members'
+  }
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    MembersComponent,
-    MessagesComponent,
-    SiteFiltersComponent
-  ],
+  declarations: [AppComponent, MembersComponent, MessagesComponent, SiteFiltersComponent],
   imports: [
     BrowserModule,
     MaterialModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
